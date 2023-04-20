d5623a007eda22ace0d9a683a7884665e9011f4a
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-20 23:49:27 +0900

    Add: Tabbar 추가

--- lib/screen/basic_appbar_tabbar_screen.dart
+++ lib/screen/basic_appbar_tabbar_screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:tabbar_example/const/tabs.dart';
 
 class BasicAppbarTabbarScreen extends StatelessWidget {
   const BasicAppbarTabbarScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [],
+    return DefaultTabController(
+      length: TABS.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('BasicAppBarScreen'),
+          bottom: TabBar(
+            tabs: TABS
+                .map(
+                  (e) => Tab(
+                    icon: Icon(
+                      e.icon,
+                    ),
+                    child: Text(
+                      e.label,
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+        body: Column(
+          children: [],
+        ),
       ),
     );
   }
