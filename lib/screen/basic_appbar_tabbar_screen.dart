6ec60c7c107945777c4f57c240f81173d29aefdb
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-20 23:28:12 +0900

    Add: screen추가

---
+++ lib/screen/basic_appbar_tabbar_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BasicAppbarTabbarScreen extends StatelessWidget {
+  const BasicAppbarTabbarScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
