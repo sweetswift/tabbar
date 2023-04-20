a9d2bf36a62d422211465daa404302224242791d
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-20 23:25:31 +0900

    Add: AppBar,ElevatedButton 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Basic Appbar TabBar Scereen'),
+          ),
+        ],
+      ),
     );
   }
 }
