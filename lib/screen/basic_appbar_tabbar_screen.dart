947ebad9454df219262bc53a5b03368d8f932a8c
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-26 23:06:38 +0900

    Add: 탭바 스크롤  옵션 추가

--- lib/screen/basic_appbar_tabbar_screen.dart
+++ lib/screen/basic_appbar_tabbar_screen.dart
@@ -11,25 +11,41 @@ class BasicAppbarTabbarScreen extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text('BasicAppBarScreen'),
-          bottom: TabBar(
-            indicatorColor: Colors.red,
-            indicatorWeight: 4.0,
-            indicatorSize: TabBarIndicatorSize.tab,
-            isScrollable: true,
-            tabs: TABS
-                .map(
-                  (e) =>
-                  Tab(
-                    icon: Icon(
-                      e.icon,
-                    ),
-                    child: Text(
-                      e.label,
-                    ),
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(100),
+            child:  Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TabBar(
+                  indicatorColor: Colors.red,
+                  indicatorWeight: 4.0,
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  isScrollable: true,
+                  labelColor: Colors.yellow,
+                  unselectedLabelColor: Colors.black,
+                  labelStyle: TextStyle(
+                    fontWeight: FontWeight.w700,
                   ),
-            )
-                .toList(),
-          ),
+                  unselectedLabelStyle: TextStyle(
+                    fontWeight: FontWeight.w100,
+                  ),
+                  tabs: TABS
+                      .map(
+                        (e) =>
+                        Tab(
+                          icon: Icon(
+                            e.icon,
+                          ),
+                          child: Text(
+                            e.label,
+                          ),
+                        ),
+                  )
+                      .toList(),
+                ),
+              ],
+            ),
+          )
         ),
         body: TabBarView(
           physics: NeverScrollableScrollPhysics(), //양옆 스크롤 막기
