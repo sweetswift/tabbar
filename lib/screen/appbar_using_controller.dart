7d904b77368c55cab49abfd8dce2cd60bed7a706
LeeChungWoo <ios.lllccww@gmail.com>
2023-04-26 23:50:22 +0900

    Add: tabController 연동

--- lib/screen/appbar_using_controller.dart
+++ lib/screen/appbar_using_controller.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:tabbar_example/const/tabs.dart';
 
-class AppBarUsingController extends StatelessWidget {
+class AppBarUsingController extends StatefulWidget {
   const AppBarUsingController({Key? key}) : super(key: key);
+
+  @override
+  State<AppBarUsingController> createState() => _AppBarUsingControllerState();
+}
+
+class _AppBarUsingControllerState extends State<AppBarUsingController>
+    with TickerProviderStateMixin {
+  late final TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    tabController = TabController(
+      length: TABS.length,
+      vsync: this,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,15 +29,12 @@ class AppBarUsingController extends StatelessWidget {
         title: Text('Appbar Using Controller'),
       ),
       body: TabBarView(
-        children: TABS
-            .map(
-              (e) => Center(
-                child: Icon(
-                  e.icon,
-                ),
-              ),
-            )
-            .toList(),
+        controller: tabController,
+        children: TABS.map((e) => Center(
+          child: Icon(
+            e.icon
+          ),
+        )).toList(),
       ),
     );
   }
